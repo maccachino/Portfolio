ba6fc2bf344a4cbe66e4bc2a91513a37ec7a355d
matija01 <matija@lioneve.com>
2023-05-15 23:57:08 +0200

    text changes

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -23,7 +23,7 @@ const About = () => {
         <div className={styles.about}>
           <div className={styles.aboutText}>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet.
+              Hello! My name is Matija and I enjoy creating things that live on the internet.
               My interest in web development started back in 2012 when I decided to try editing custom Tumblr themes
               — turns out hacking together a custom reblog button taught me a lot about HTML & CSS!
               <br />
